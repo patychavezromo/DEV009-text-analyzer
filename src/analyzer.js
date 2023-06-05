1c1d19b960855de95908564344ec10f27c66234d
Paty Chavez <patricia.araceli.chavez.romo@gmail.com>
2023-06-05 15:52:29 -0600

    Agregado métrico para contar caracteres

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,9 +1,12 @@
 const analyzer = {  
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    let totalcaracteres = text.length;
+    return totalcaracteres;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+let textarea = document.querySelector("textarea");
+textarea.addEventListener('keyup', (event) =>{
+    let text = event.target.value;
+    //console.log(text);
+    let characterCount= analyzer.getCharacterCount(text);
+    //console.log(characterCount);
+    document.getElementById("caracteres").innerHTML=characterCount;
+
+})
