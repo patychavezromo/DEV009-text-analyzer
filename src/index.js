5e4513d3a79b9f3aa0b424ed00163c98734b0cb1
Paty Chavez <patricia.araceli.chavez.romo@gmail.com>
2023-06-09 07:38:45 -0600

    Agregado funcionalidad al botón para restablecer métrica a 0

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import analyzer from './analyzer.js';
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 let textarea = document.querySelector("textarea");
 textarea.addEventListener('keyup', (event) =>{
-    let text = event.target.value;
-    //console.log(text);
-    let characterCount= analyzer.getCharacterCount(text);
-    //console.log(characterCount);
-    document.getElementById("caracteres").innerHTML=characterCount;
-
+let text = event.target.value;
+//console.log(text);
+let characterCount= analyzer.getCharacterCount(text);
+//console.log(characterCount);
+document.getElementById("caracteres").innerHTML=characterCount;
 });
 
 let button =document.getElementById("reset-button");
-button.addEventListener("click", ()=>{
+    button.addEventListener("click", ()=>{
     let textarea = document.querySelector("textarea");
-    textarea.value = '';
+    textarea.value ='';
+    document.getElementById("caracteres").innerHTML='';
 });
