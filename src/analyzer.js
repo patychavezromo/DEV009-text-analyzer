8e10b76ee251cea736f138b2fdf350f319182c9e
Paty Chavez <patricia.araceli.chavez.romo@gmail.com>
2023-06-12 10:36:58 -0600

    Agregada funcionalidad para contar palabras y modificaciones a la lista de metricos

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,15 +1,27 @@
 const analyzer = {  
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-    
+    let arrayStringOrigin= text.split(' ');
+    let filteredArray= [];
+    for(let i=0; i<arrayStringOrigin.length; i++){
+      let currentString= arrayStringOrigin[i];
+      if(currentString.length>0){
+        filteredArray.push(currentString);      
+      }
+    }
+    let totalWords= filteredArray.length;
+    return totalWords;
   },
+
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
-    let totalcaracteres = text.length;
-    return totalcaracteres;
+    let totalcharacteres = text.length;
+    return totalcharacteres;
   },
+
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 let textarea = document.querySelector("textarea");
+
 textarea.addEventListener('keyup', (event) =>{
-let text = event.target.value;
-//console.log(text);
-let characterCount= analyzer.getCharacterCount(text);
-//console.log(characterCount);
-document.getElementById("characters").innerHTML=characterCount;
+    let text = event.target.value;
+    let characterCount= analyzer.getCharacterCount(text);
+    document.getElementById("characters").innerHTML=characterCount;
+
+    let wordCount=analyzer.getWordCount(text);
+    document.getElementById("words").innerHTML=wordCount;
 });
+
+
 
 let button =document.getElementById("reset-button");
     button.addEventListener("click", ()=>{
     let textarea = document.querySelector("textarea");
     textarea.value ='';
     document.getElementById("characters").innerHTML='0';
+    document.getElementById("words").innerHTML='0';
 });
